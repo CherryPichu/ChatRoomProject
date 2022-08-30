72e442ac6594e4122f430a8a3a0eb1048a0d9a7c
CherryPichu <uskawjdu@gmail.com>
2022-08-30 21:08:57 +0900

    first commit

---
+++ Tech-Document.md
@@ -0,0 +1,20 @@
+출처 : https://forceson.github.io/android/%EC%B1%84%ED%8C%85%EC%95%B1%EC%9D%84-%EC%84%A4%EA%B3%84%ED%95%98%EB%A9%B0-%EB%B0%B0%EC%9A%B4-%EB%82%B4%EC%9A%A9/
+
+출처2 : https://sendbird.com/ko/blog/%EC%9E%90%EC%B2%B4-or-%EC%99%B8%EC%A3%BC%EA%B0%9C%EB%B0%9C-%EC%B9%B4%ED%86%A1%EA%B3%BC-%EA%B0%99%EC%9D%80-%EB%A9%94%EC%8B%A0%EC%A0%80-%EC%96%B4%ED%94%8C%EC%9D%84-%EA%B0%9C%EB%B0%9C%ED%95%98%EA%B8%B0
+
+### 개발한 내용
+
+
+### 기술 요건 검토
+
+- Server Operaation : 자체서버
+- Traffic handling : Load Balancing
+- Database : No SQL(MongoDB), RDB (MySQL, Postgres)
+- Network layer : WebScoket, HTTP, TCP socket
+- client plaform : Web(JavaScript)
+- 기타 : Logging, Monitoring, Analytics, Swagger
+
+
+### 
+
+
---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const morgan = require('morgan')
+const dotenv = require('dotenv')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+dotenv.config();
+
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'views/css'))) // 정적파일 제공
+app.use(express.static(path.join(__dirname, 'views/js'))) // 정적파일 제공
+
+app.set('port', process.env.PORT || 8082)
+
+app.use(morgan('dev'))
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, './views', 'Chat_waiting.html'))
+})
+
+
+var options = {
+    swaggerOptions: {
+      validatorUrl: null
+    }
+  };
+// app.use('')
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+// swagger api 문서
+
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기중')
+})
+
+
+
